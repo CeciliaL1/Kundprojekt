c167f8fca40cd93a9129e588814cfb9c6b520b01
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-10-14 16:46:28 +0200

    adding js-file

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const toTheMoon = document.getElementsByClassName('.to-the-moon');
+const menuBtn = document.querySelector('.menu-button');
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('menu-open');
+
+});
